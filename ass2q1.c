b77727d3e539647d6cbe5647046f79573ac3aef6
Abhimanyu1221 <165375245+Abhimanyu1221@users.noreply.github.com>
2025-05-10 23:51:08 +0530

    Create ass2q1.c

---
+++ ass2q1.c
@@ -0,0 +1,19 @@
+//Accept one number from user and print that number of * on screen. 
+void Display(int iNo) 
+{ 
+int iCnt = 0; 
+// Write Updater 
+while(iCnt < ) 
+{ 
+printf(" * "); 
+; 
+} 
+} 
+int main() 
+{ 
+int iValue = 0; 
+printf("Enter number"); 
+scanf(" "& ); 
+Display (iValue); 
+return 0; 
+}
